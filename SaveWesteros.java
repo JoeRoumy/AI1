f8f707204f2b6cbbde5b1ac2953c5a8afad4c951
JoeRoumy <JoeRoumy@users.noreply.github.com>
2018-10-02 16:55:25 +0200

    toot

--- SaveWesteros.java
+++ SaveWesteros.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 /*
  * need to consider repeated states
- * keep nodes from being deleted after dequeue
+ * keep nodes from being deleted after dequeue >>>> parent nodes
  * heuristic considers number of white walkers
  * 
  * */
@@ -17,7 +17,7 @@ public class SaveWesteros extends SearchProblem {
 	public int expandedNodes;
 	
 	
-	private Solution BF(Grid grid) {
+	private ArrayList<Snode> BF(Grid grid) {
 		// TODO Auto-generated method stub
 		return null;
 
@@ -40,84 +40,85 @@ public class SaveWesteros extends SearchProblem {
 
 	}
 
-	private Solution UC(Grid grid) {
+	private ArrayList<Snode> UC(ArrayList<Snode>nodes,Snode[] n) {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+	private ArrayList<Snode> GR1(Grid grid) {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+	private ArrayList<Snode> GR2(Grid grid) {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+	private ArrayList<Snode> AS1(Grid grid) {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+	private ArrayList<Snode> AS2(Grid grid) {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+
+	private void Search(Grid grid, String strategy, Boolean visualize) {
+		//solution
+		Snode leaf = null;
 		
 		//initialization
-		Solution sol = new Solution(100);
 		queue = new ArrayList<Snode>(500);
 		
 		//instantiating the queue
 		queue.add(new Snode(initState));
+			
 		
 		while (queue.size()>0) {
 			Snode thisNode = queue.remove(0);
-//			if(goalTest.call(thisNode.state)) {
-//				
-//			}
-			
-		}
+			if(thisNode.state.isGoal) {
+				
+			}
+
+			switch (strategy) {
+			case "BF":	queue = BF(grid);		break;
+			case "DF":	queue = DF(queue,expand(thisNode));		break;
+//			case "ID":	queue = ID(queue,expand(thisNode));		break;
+			case "UC":	queue = UC(queue,expand(thisNode));		break;
+			case "GR1":	queue = GR1(grid);		break;
+			case "GR2":	queue = GR2(grid);		break;
+			case "AS1":	queue = AS1(grid);		break;
+			case "AS2":	queue = AS2(grid);		break;
+			default: System.out.println("Invalid search strategy "+strategy);			return;
+			}
 		
-		return null;
-
-	}
-
-	private Solution GR1(Grid grid) {
-		// TODO Auto-generated method stub
-		return null;
-
-	}
-
-	private Solution GR2(Grid grid) {
-		// TODO Auto-generated method stub
-		return null;
-
-	}
-
-	private Solution AS1(Grid grid) {
-		// TODO Auto-generated method stub
-		return null;
-
-	}
-
-	private Solution AS2(Grid grid) {
-		// TODO Auto-generated method stub
-		return null;
-
-	}
-
-	private Grid GenGrid() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private void Search(Grid grid, String strategy, Boolean visualize) {
-		Solution solution = null;
-		switch (strategy) {
-		case "BF":	solution = BF(grid);		break;
-//		case "DF":	solution = DF(grid);		break;
-//		case "ID":	solution = ID(grid);		break;
-		case "UC":	solution = UC(grid);		break;
-		case "GR1":	solution = GR1(grid);		break;
-		case "GR2":	solution = GR2(grid);		break;
-		case "AS1":	solution = AS1(grid);		break;
-		case "AS2":	solution = AS2(grid);		break;
-		default: System.out.println("Invalid search strategy "+strategy);			return;
 		}
 		
 		if(visualize) {
-			PrintSolution(solution);
+			PrintSolution(grid, leaf);
 		}
 		
 		System.out.println("Nodes expanded: "+expandedNodes);
 	}
 	
 	
-	private void PrintSolution(Solution solution) {
+	private void PrintSolution(Grid grid, Snode leaf) {
 		// TODO Auto-generated method stub
 
 	}
 	
-	
+	private Snode[] expand(Snode node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
